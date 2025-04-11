6b5a17279c6533193b31a8e5db24f36b9768bf1b
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-04-11 14:31:53 -0300

    faz o eli

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 void main(){
-
-
-    
+    printf("Faz o L");
+    int ol;
+    scanf("%d",&ol);
+    printf("O L que você fez tem um poder de %d!!!!!!!!!!!!!!!!!!!!!!!!!!!!",ol)
 }
