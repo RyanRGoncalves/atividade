e301594e61f7194fef2682bfd90c56c0f118c741
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-04-11 15:23:58 -0300

    notas

--- main.c
+++ main.c
@@ -2,5 +2,20 @@
 #include <stdlib.h>
 
 void main() {
-    int value;
+    int n1;
+    int n2;
+    int n3;
+    int M;
+    printf("Qual a primeira nota");
+    scanf("%d",n1);
+    printf("Qual a segunda nota");
+    scanf("%d",n2);
+    printf("Qual a terceira nota");
+    scanf("%d",n3);
+    M = (n1+n2+n3)/3;
+    if () {
+        
+    } else {
+
+    }
 }
