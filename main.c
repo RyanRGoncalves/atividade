52785cf8a2786f12fc78e99e05ef6acff8df2d8f
RyanRG <ryan.goncalves19@etec.sp.gov.br>
2025-04-11 15:28:27 -0300

    continuação das notas

--- main.c
+++ main.c
@@ -6,16 +6,16 @@ void main() {
     int n2;
     int n3;
     int M;
-    printf("Qual a primeira nota");
-    scanf("%d",n1);
-    printf("Qual a segunda nota");
-    scanf("%d",n2);
-    printf("Qual a terceira nota");
-    scanf("%d",n3);
+    printf("Qual a primeira nota\n");
+    scanf("%d",&n1);
+    printf("Qual a segunda nota\n");
+    scanf("%d",&n2);
+    printf("Qual a terceira nota\n");
+    scanf("%d",&n3);
     M = (n1+n2+n3)/3;
-    if () {
-        
+    if (M > 7) {
+        printf("Com uma nota de %d, o aluno passou!\n",M);
     } else {
-
+        printf("Com uma not de %d, o aluno reprovou.\n",M);
     }
 }
